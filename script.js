14bcdd1dbf2b1045bd20723f934b5281fcb95803
Suraj Kumar <surajkrstm4002@gmail.com>
2025-09-08 16:21:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "AIzaSyCX9jK1NVJwc90ctA4TI5CXPwEbgfGRM28";
+const GOOGLE_API_KEY = "AIzaSyBeiUS4fuCQbA52jacz2_ijuOTgMhR5DC0";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
